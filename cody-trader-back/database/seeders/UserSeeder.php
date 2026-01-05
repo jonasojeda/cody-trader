aa356933149583000c298593d79ecfc67c573529
jonas ojeda <jonojed98@gmail.com>
2026-01-05 19:03:34 -0300

    Initial commit back

---
+++ cody-trader-back/database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@yopmail.com',
+                'password' => '1234'
+            ]
+        ];
+
+        foreach ($users as $user) {
+            if (!User::where('email', $user['email'])->exists()) {
+                User::create([
+                    'name' => $user['name'],
+                    'email' => $user['email'],
+                    'password' => Hash::make($user['password']),
+                ]);
+            }
+        }
+    }
+}
