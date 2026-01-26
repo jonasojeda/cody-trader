8e66973a9913019800c4c451ac4d8a43cdbf48b1
jonas ojeda <jonojed98@gmail.com>
2026-01-26 17:33:07 -0300

    feat: Implementacion de Modelo y migracion de Course

---
+++ back/app/Models/Course.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Course extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'thumbnail',
+    ];
+
+    //Relaciones
+    //Funciones publicas
+    public function obtenerDatos()
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'thumbnail' => $this->thumbnail,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
